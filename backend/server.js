50c034b946f40af028a5cdba1ad0723a04fb2731
Casey Morris <morrisacasey@gmail.com>
2024-06-07 15:27:30 -0400

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,51 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const citationRoutes = require('./routes/citations')
-const userRoutes = require('./routes/user')
+const express = require('express');
+const mongoose = require('mongoose');
+const citationRoutes = require('./routes/citations');
+const userRoutes = require('./routes/user');
 const favoriteRoutes = require('./routes/favorites');
 const searchRoutes = require('./routes/search');
 const cors = require('cors');
 
 // express app
-const app = express()
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
+  console.log(req.path, req.method);
   next();
-})
+});
 
 // routes
-app.use('/api/citations', citationRoutes)
-app.use('/api/user', userRoutes)
-app.use('/api/favorites', favoriteRoutes); 
+app.use('/api/citations', citationRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/favorites', favoriteRoutes);
 app.use('/api/search', searchRoutes);
 
+// start the server
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    // listen for requests
-    app.listen(process.env.PORT || 4000, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+    console.log('Connected to MongoDB');
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log('Error connecting to MongoDB:', error);
+  });
+
+// handle server errors
+server.on('error', (error) => {
+  console.error('Server error:', error);
+});
